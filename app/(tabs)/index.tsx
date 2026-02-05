295e70171b53f0041bf43bc133c0377c25ea3d78
Ryan Cleeton <wrc089@gmail.com>
2026-02-04 19:08:42 -0500

    fixed deprecating warning

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { SwipeCard } from '../../components/SwipeCard';
 import { ThemedText } from '../../components/ThemedText';
 import { useDerivedData, useFoodData } from '../../hooks/useFoodData';
